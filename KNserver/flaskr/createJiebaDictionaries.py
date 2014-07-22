ab48466c3143da80d2cc915c1f2c97d22cd13fa0
KyleCrop <KyleCrop@users.noreply.github.com>
2014-07-22 14:52:00 +0800

    Update createJiebaDictionaries.py

--- KNserver/flaskr/createJiebaDictionaries.py
+++ KNserver/flaskr/createJiebaDictionaries.py
@@ -1,52 +1,62 @@
 import os
 from datetime import datetime
-#import icu
 from chardet import detect
 
 cities = []
 """THIS IS THE LINE THAT GRABS THE CITY NAMES, DON'T UNCOMMENT OR RISK CREATING 4037 FILES"""
 #parsedCities = open("Parsed_Cities.txt", "r")
 
-for line in parsedCities :
-	stopAt = line.find(":")
-	cities.append(line[:stopAt])
-parsedCities.close()
+def createDicts() :
+	for line in parsedCities :
+		stopAt = line.find(":")
+		cities.append(line[:stopAt])
+	parsedCities.close()
 
-dictionary = ""
-jiebaDict = open("dict.txt", "r")
-for line in jiebaDict :
-	dictionary += line
+	dictionary = ""
+	jiebaDict = open("dict.txt", "r")
+	for line in jiebaDict :
+		dictionary += line
 
-#Hard-coding categories because assuming they'll stay constant	
-categories = ["ershou", "qiuzhi", "cheliang", "jianzhi", "fangwu", "jiaoyou", "chongwu", "zhaopin", "fuwu", "jianliku", "peixun"]
+	# Hard-coding categories because assuming they'll stay constant	
+	categories = ["ershou", "qiuzhi", "cheliang", "jianzhi", "fangwu", "jiaoyou", "chongwu", "zhaopin", "fuwu", "jianliku", "peixun"]
 
-#For speed-testing
-timeA = datetime.now()
-print timeA
-
-#NEED TO SPECIFY ENTIRE PATH FROM ROOT, so change 'kyle' to whatever necessary
-for city in cities :
-	path = "/home/kyle/Documents/jieba/KNserver/flaskr/dictionaries/" + city
-	os.mkdir(path)
-	
 	"""
-	Need to make directories within this directory for each city,
-	put each dictionary into the correct /dictionaries/city directory.
-	Also test that parsing "city" works (Shanghai:2i3422uih3)
+	#For speed-testing
+	timeA = datetime.now()
+	print timeA
 	"""
+
+	# Create "dictionaries" directory if not already created, NEED TO SPECIFY PATH FROM ROOT
+	try: path = "/home/kyle/Documents/jieba/KNserver/flaskr/dictionaries"
+		os.mkdir(path)
+	except:
+		pass
+
+	# NEED TO SPECIFY ENTIRE PATH FROM ROOT, so change 'kyle' to whatever necessary
+	for city in cities :
+		path = "/home/kyle/Documents/jieba/KNserver/flaskr/dictionaries/" + city
+		os.mkdir(path)
 	
-	for category in categories :
-		fileName = "/home/kyle/Documents/jieba/KNserver/flaskr/dictionaries/" + city + "/" + city + "_" + category
-		newDict = open(fileName, "w")
-		newDict.write(dictionary)
-		newDict.close()
+		"""
+		Need to make to make a separate directory for each city within this directory
+		put each dictionary into the correct /dictionaries/city directory.
+		"""
+	
+		for category in categories :
+			fileName = "/home/kyle/Documents/jieba/KNserver/flaskr/dictionaries/" + city + "/" + city + "_" + category
+			newDict = open(fileName, "w")
+			newDict.write(dictionary)
+			newDict.close()
 
-'''#For speed-testing
-timeB = datetime.now()
-print timeB
+	"""
+	#For speed-testing
+	timeB = datetime.now()
+	print timeB
 
-timeE = timeB - timeA
-print timeE.seconds '''
+	timeE = timeB - timeA
+	print timeE.seconds
+	"""
+
 
 def makeUniform(originalDictPath,newDictPath):
 	"""Procedure: Converts all entries, if possible, in given dictionary to
